8426a11969b6aafe19c77dae21d5393de8685b58
kodeman2 <holuwakoladey@gmail.com>
2022-10-26 21:43:07 +0100

    updated footer

--- HNG9_first_task/src/components/Footer.jsx
+++ HNG9_first_task/src/components/Footer.jsx
@@ -2,6 +2,12 @@ import "./footer.css";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+<div className="footercontainer">
+  <img src='..\images\zuri.svg' alt=""  />
+  <p>HNG Internship 9 Frontend Task</p>
+  <img src='..\images\i4g.svg' alt=""  />
+</div>
+    </div>
   )
 }
